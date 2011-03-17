92562f107e825d8dc1dd22af537bc9fa56e767b7
Daniel Mack <zonque@gmail.com>
2011-03-17 14:54:11 +0100

    framework: further refinements of library relocation tricks

--- PulseAudio.framework/mkFramework.sh
+++ PulseAudio.framework/mkFramework.sh
@@ -66,38 +66,45 @@ cp -r $staging/lib/pulse-$shortversion/modules $output/Versions/A/Resources/
 
 # resolve library dependencies
 
-libpath=$output/Versions/A/Resources/
+function relocate_libs()
+{
+	pattern=$1; shift
+	prefix=$1; shift
+	path=$1; shift
 
-# as the resolving process can raise new dependencies to /opt/local/...,
-# we have to do this in a loop
+	# as the resolving process can raise new dependencies, we have to do this in a loop
 
-while [ "$(otool -L $libpath/*.dylib | grep /opt/local)" ]; do
-	for lib in $libpath/*.dylib; do
-		for deplib in $(otool -L $lib | grep /opt/local | cut -d" " -f1); do
-			baselib=$(basename $deplib)
-			echo "resolving dependency $deplib for $baselib ..."
-			test -f $libpath/$baselib || cp $deplib $libpath/
-			install_name_tool -id "@loader_path/$baselib" $lib
-			install_name_tool -change $deplib "@loader_path/$baselib" $lib
+	while [ "$(find $path -type f -depth 1 | xargs otool -L | grep $pattern)" ]; do
+		for lib in $(find $path -type f -depth 1); do
+			for deplib in $(otool -L $lib | grep $pattern | cut -d" " -f1); do
+				baselib=$(basename $deplib)
+				echo "resolving dependency $deplib for $baselib ..."
+				test -f $libpath/$baselib || cp $deplib $libpath/
+				install_name_tool -id $prefix/$baselib $lib
+				install_name_tool -change $deplib $prefix/$baselib $lib
 
-			# check if it worked
-			if [ "$(otool -L $lib | grep $deplib)" ]; then
-				echo "ERROR. Cannot change name $deplib in $lib"
-				echo "You might need to build this library with -headerpad_max_install_names"
-				exit 1;
-			fi
+				# check if it worked
+				if [ "$(otool -L $lib | grep $deplib)" ]; then
+					echo "ERROR. Cannot change name $deplib in $lib"
+					echo "You might need to build this library with -headerpad_max_install_names"
+					exit 1;
+				fi
+			done
 		done
 	done
-done
+}
 
+libpath=$output/Versions/A/Resources/
+modpath=$libpath/modules/
 binpath=$libpath/bin/
+portlibs=/opt/local/lib
 
-for bin in $binpath/*; do
-	for deplib in 	\
-		$(otool -L $bin | grep /opt/local | cut -d" " -f1)	\
-		$(otool -L $bin | grep libpulse | cut -d" " -f1); do
-			baselib=$(basename $deplib)
-			install_name_tool -change $deplib "@executable_path/../$baselib" $bin
-	done
+for iter in 1 2; do
+	relocate_libs $portlibs		@loader_path 		$libpath
+	relocate_libs $staging		@loader_path 		$libpath
+	relocate_libs $portlibs		@loader_path/.. 	$modpath
+	relocate_libs $staging		@loader_path/..		$modpath
+	relocate_libs $portlibs		@executable_path/..	$binpath
+	relocate_libs $staging		@executable_path/..	$binpath
 done
 
