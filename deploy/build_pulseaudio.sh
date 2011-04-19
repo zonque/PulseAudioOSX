0b3b421deac14670737ec76599e10ca04bf8cade
Daniel Mack <zonque@gmail.com>
2011-04-19 13:34:17 +0200

    deploy: compile with -O2

--- deploy/build_pulseaudio.sh
+++ deploy/build_pulseaudio.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+#
+# This needs to be in its own script, as the other parts of the build system
+# don't like to have environment variables redefined
+#
+
 export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 export CC="gcc-4.2"
-export CFLAGS="-I/opt/local/include -O0 -g"
+#export CFLAGS="-I/opt/local/include -O0 -g"
+export CFLAGS="-I/opt/local/include -O2"
 export LDFLAGS="-L/opt/local/lib"
 staging=/Library/Frameworks/pulse.framework/
 
