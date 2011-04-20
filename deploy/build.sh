15fd17df9f4020cce4984e2f903dea8486687a66
Daniel Mack <zonque@gmail.com>
2011-04-20 09:59:08 +0200

    deploy: do not treat PulseAudioHelper.app as StartupItem

--- deploy/build.sh
+++ deploy/build.sh
@@ -26,38 +26,36 @@ sh ${base}/build_pulseaudio.sh
 ################################## framework ##################################
 cd ${base}/../Framework
 ./fixupFramework.sh
-dest=${targetdir}/Library/Frameworks/
+dest=${targetdir}/Library/Frameworks
 mkdir -p ${dest}
-cp -a /Library/Frameworks/pulse.framework ${dest}
-
-################################## HAL plugin ##################################
-cd ${base}/../HALPlugin/
-XcodeBuild HALPlugin.xcodeproj
-dest=${targetdir}/Library/Audio/Plugins/HAL/
-mkdir -p ${dest}
-cp -a build/Release/PulseAudio.plugin ${dest}
-
-################################## Preference Pane ##################################
-cd ${base}/../PreferencePane/
-XcodeBuild PAPreferencePane.xcodeproj
-dest=${targetdir}/Library/PreferencePanes/
-mkdir -p ${dest}
-cp -a build/Release/PulseAudio.prefPane ${dest}
+cp -a /Library/Frameworks/pulse.framework ${dest}/
 
 ################################## PulseAudioHelper ##################################
 cd ${base}/../PulseAudioHelper/
 XcodeBuild PulseAudioHelper.xcodeproj
-dest=${targetdir}/Library/StartupItems/PulseAudioHelper
+dest=${targetdir}/Library/Frameworks/pulse.framework/bin
+cp -a build/Release/PulseAudioHelper ${dest}/
+
+################################## HAL plugin ##################################
+cd ${base}/../HALPlugin/
+XcodeBuild HALPlugin.xcodeproj
+dest=${targetdir}/Library/Audio/Plugins/HAL
 mkdir -p ${dest}
-cp -a build/Release/PulseAudioHelper ${dest}
-cp -a StartupParameters.plist ${dest}
+cp -a build/Release/PulseAudio.plugin ${dest}/
+
+################################## Preference Pane ##################################
+cd ${base}/../PreferencePane/
+XcodeBuild PAPreferencePane.xcodeproj
+dest=${targetdir}/Library/PreferencePanes
+mkdir -p ${dest}
+cp -a build/Release/PulseAudio.prefPane ${dest}/
 
 ################################## PulseConsole ##################################
 cd ${base}/../PulseConsole/
 XcodeBuild PulseConsole.xcodeproj
-dest=${targetdir}/Applications/
+dest=${targetdir}/Applications
 mkdir -p ${dest}
-cp -a build/Release/PulseConsole.app ${dest}
+cp -a build/Release/PulseConsole.app ${dest}/
 
 
 echo "Built and staged into ${targetdir}"
