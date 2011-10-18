a308fa2f3f866cec09ace9f632863f545118d8cc
Daniel Mack <zonque@gmail.com>
2011-10-18 12:35:05 +0200

    deploy: added bootstrap_machine.sh

---
+++ deploy/bootstrap_machine.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PORTS="autoconf automake intltool libtool libsndfile speex-devel gdbm liboil json-c; do sudo port install"
+
+for port in $PORTS; do
+	port install $port +universal;
+done
+
+# we need to stage the pulseaudio binaries to /Library/Frameworks/PulseAudio.framework, hence
+# this path needs to be writable by the current user. This is admittedly a hack, but I lack a
+# better solution for now.
+
+chown $USER /Library/Frameworks
+
